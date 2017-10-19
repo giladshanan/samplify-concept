3241506b01811a2c8e725b8f7bb4b5b7a6d127e4
Ryan McCool <email": null>
2017-10-19 18:59:57 -0500

    add playlists index, .env files; basic functionality achieved

--- Gemfile
+++ Gemfile
@@ -36,6 +36,9 @@ gem 'jbuilder', '~> 2.5'
 # Ruby wrapper for Spotify
 gem 'rspotify'
 
+# dotenv
+gem 'dotenv-rails', groups: [:development, :test]
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,41 +1,46 @@
 class PlaylistsController < ApplicationController
 
-  # def new
-  # end
+  def index
+    RSpotify.authenticate(ENV["spotify_client_id"], ENV["spotify_secret_id"])
+    me = RSpotify::User.find('ryanmccool13')
+    me.playlists.each do |playlist|
+      puts playlist.name
+    end
+     puts me.playlists[0].tracks.first.preview_url
+     puts me.playlists[0].tracks.first.album.images
+  end
 
-  RSpotify.authenticate("<your_client_id>", "<your_client_secret>")
+# # Now you can access playlists in detail, browse featured content and more
 
-# Now you can access playlists in detail, browse featured content and more
 
-me = RSpotify::User.find('guilhermesad')
-me.playlists #=> (Playlist array)
+# me.playlists #=> (Playlist array)
 
-# Find by id
-playlist = RSpotify::Playlist.find('guilhermesad', '1Xi8mgiuHHPLQYOw2Q16xv')
-playlist.name               #=> "d33p"
-playlist.description        #=> "d33p h0uz"
-playlist.followers['total'] #=> 1
-playlist.tracks             #=> (Track array)
+# # Find by id
+# playlist = RSpotify::Playlist.find('guilhermesad', '1Xi8mgiuHHPLQYOw2Q16xv')
+# playlist.name               #=> "d33p"
+# playlist.description        #=> "d33p h0uz"
+# playlist.followers['total'] #=> 1
+# playlist.tracks             #=> (Track array)
 
-# Search by category
-party = RSpotify::Category.find('party')
-party.playlists #=> (Playlist array)
-categories = RSpotify::Category.list # See all available categories
+# # Search by category
+# party = RSpotify::Category.find('party')
+# party.playlists #=> (Playlist array)
+# categories = RSpotify::Category.list # See all available categories
 
-# Access featured content from Spotify's Browse tab
-featured_playlists = RSpotify::Playlist.browse_featured(country: 'US')
-new_releases = RSpotify::Album.new_releases(country: 'ES')
+# # Access featured content from Spotify's Browse tab
+# featured_playlists = RSpotify::Playlist.browse_featured(country: 'US')
+# new_releases = RSpotify::Album.new_releases(country: 'ES')
 
-# Access tracks' audio features
-sorry = RSpotify::Track.search("Sorry").first
-sorry.audio_features.danceability #=> 0.605
-sorry.audio_features.energy #=> 0.768
-sorry.audio_features.tempo #=> 100.209
+# # Access tracks' audio features
+# sorry = RSpotify::Track.search("Sorry").first
+# sorry.audio_features.danceability #=> 0.605
+# sorry.audio_features.energy #=> 0.768
+# sorry.audio_features.tempo #=> 100.209
 
-# Get recommendations
-recommendations = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
-recommendations = RSpotify::Recommendations.generate(seed_tracks: my_fav_tracks.map(&:id))
-recommendations = RSpotify::Recommendations.generate(seed_artists: my_fav_artists.map(&:id))
-recommendations.tracks #=> (Track array)
+# # Get recommendations
+# recommendations = RSpotify::Recommendations.generate(seed_genres: ['blues', 'country'])
+# recommendations = RSpotify::Recommendations.generate(seed_tracks: my_fav_tracks.map(&:id))
+# recommendations = RSpotify::Recommendations.generate(seed_artists: my_fav_artists.map(&:id))
+# recommendations.tracks #=> (Track array)
 
 end
